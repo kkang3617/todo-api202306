5dd79fc3b04898b3c850fecc2a4464281a99d6a8
kkang3617 <rkdgus3617@naver.com>
2023-06-23 10:54:01 +0900

    User 회원가입 컨트롤러

--- src/main/java/com/example/todo/userapi/service/UserService.java
+++ src/main/java/com/example/todo/userapi/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.todo.userapi.service;
 
+import com.example.todo.exception.DuplicatedEmailException;
+import com.example.todo.exception.NoRegisteredArgumentsException;
 import com.example.todo.userapi.dto.UserSignUpResponseDTO;
 import com.example.todo.userapi.entity.User;
 import com.example.todo.userapi.repository.UserRepository;
@@ -18,16 +20,17 @@ public class UserService {
     private final PasswordEncoder encoder;
 
     //회원 가입 처리
-    public UserSignUpResponseDTO create(UserRequestSignUpDTO dto) {
+    public UserSignUpResponseDTO create(UserRequestSignUpDTO dto)
+        throws RuntimeException {
 
         String email = dto.getEmail();
         if(dto == null) {
-            throw new RuntimeException("가입 정보가 없습니다.");
+            throw new NoRegisteredArgumentsException("가입 정보가 없습니다.");
         }
 
         if(userRepository.existsByEmail(email)) {
             log.warn("이메일이 중복되었습니다. - {}", email);
-            throw new RuntimeException("중복된 이메일 입니다.");
+            throw new DuplicatedEmailException("중복된 이메일 입니다.");
         }
         
         //패스워드 인코딩
@@ -49,6 +52,14 @@ public class UserService {
         return userRepository.existsByEmail(email);
     }
 }
+
+
+
+
+
+
+
+
 
 
 
