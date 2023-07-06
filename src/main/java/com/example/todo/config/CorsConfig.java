df7974d69cf850c872968c85d44a317ee5f34e44
kkang3617 <rkdgus3617@naver.com>
2023-07-06 12:50:18 +0900

    DAY95 07.06. hotfix/3. cross origin config3

--- src/main/java/com/example/todo/config/CorsConfig.java
+++ src/main/java/com/example/todo/config/CorsConfig.java
@@ -10,14 +10,14 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override //addcors 만 치면 자동완성 뜸.
     public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/api/**") //어떤 요청에 대해서 허용할지?
+//        registry
+//                .addMapping("/api/**") //어떤 요청에 대해서 허용할지?
 //                .allowedOrigins("http://localhost:3000") //어떤 클라이언트를 허용할지
-                .allowedMethods("*")
-                .allowedMethods("*") //어떤 요청방식을 허용할지
-                .allowedHeaders("*") //어떤 요청 헤더를 허용할지
-                .allowCredentials(true) //쿠키 전달을 허용할 것인지
-                .maxAge(3600) //캐싱 시간을 설정(초 단위)
-                ;
+//                .allowedMethods("*")
+//                .allowedMethods("*") //어떤 요청방식을 허용할지
+//                .allowedHeaders("*") //어떤 요청 헤더를 허용할지
+//                .allowCredentials(true) //쿠키 전달을 허용할 것인지
+//                .maxAge(3600) //캐싱 시간을 설정(초 단위)
+//                ;
     }
 }
